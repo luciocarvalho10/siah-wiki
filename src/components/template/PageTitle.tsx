9b6d3f088aa12691e001cf8264e79e7fd1b70f34
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-05-25 22:32:20 -0400

    paginas admin e home

--- src/components/template/PageTitle.tsx
+++ src/components/template/PageTitle.tsx
@@ -7,11 +7,11 @@ interface IPageTitleProps {
 export default function PageTitle(props: IPageTitleProps) {
 	return (
 		<div>
-			<h1 className="m-0">
+			<h1 className="flex m-0">
 				{props.icon ? props.icon : false}
 				{props.main}
 			</h1>
-			<h2 className="mt-[5px] text-[1.5rem] text-gray-400">
+			<h2 className="mt-[5px] text-[1.3rem] text-gray-400">
 				{props.sub}
 			</h2>
 			<hr />
