a9f23295989f9d456eb0acb6c4b0a108f8616674
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-05-26 01:11:06 -0400

    refatorando layout das paginas

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,12 @@
-import Layout from '@/components/template/Layout'
 import PageTitle from '@/components/template/PageTitle'
 import { HomeIcon } from '@heroicons/react/16/solid'
 
 export default function Home() {
 	return (
-		<Layout>
-			<PageTitle
-				icon={<HomeIcon className="h-6 w-6" />}
-				main="Dashboard"
-				sub="SiahWiki"
-			/>
-		</Layout>
+		<PageTitle
+			icon={<HomeIcon className="h-6 w-6 mr-2" />}
+			main="Dashboard"
+			sub="SiahWiki"
+		/>
 	)
 }
