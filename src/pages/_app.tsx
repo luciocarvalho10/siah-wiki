7d37cf8d78153ad3c83cb6c4d62af0accb400b64
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-06-02 01:00:26 -0400

    Corrigindo font e medida do icone

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,26 @@ import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
 import { StoreContext } from '@/data/context/StoreConext'
 import Layout from '@/components/template/Layout'
+import { Lato }from 'next/font/google'
+
+const lato = Lato({
+  weight: ['100', '300', '400', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<StoreContext>
+			<style
+				jsx
+				global
+			>{`
+				* {
+					font-family: ${lato.style.fontFamily};
+				}
+			`}</style>
+
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
