967dd307c4298314ed21daa6ca4af0a0d94fb220
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2024-06-12 00:42:15 -0400

    Acresentando componente Stats

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,15 +1,27 @@
 import PageTitle from '@/components/pages/PageTitle'
 import Stat from '@/components/pages/Stat'
+import { Stats } from '@/data/db/stats'
 import { baseApiUrl } from '@/globais'
-import { HomeIcon } from '@heroicons/react/16/solid'
+import { HomeIcon, FolderIcon, NewspaperIcon, UserIcon } from '@heroicons/react/16/solid'
 import { useEffect, useState } from 'react'
 
 export default function Dashboard() {
-    const [stats, setStats] = useState({})
-
-    function getStats() {
-
+    const statsEmpty: Stats = {
+        users: 0,
+        articles: 0,
+        categories: 0,
     }
+    const [stats, setStats] = useState<Stats>(statsEmpty)
+
+    async function getStats() {
+		const res = await fetch(`${baseApiUrl}/stats`)
+        const stats: Stats = await res.json()
+        setStats(stats)
+	}
+
+    useEffect(() => {
+        getStats()
+    }, [])
 
 	return (
 		<div>
@@ -18,6 +30,26 @@ export default function Dashboard() {
 				main="Dashboard"
 				sub="SiahWiki"
 			/>
+			<div className="stats flex flex-wrap justify-between">
+				<Stat
+					title="Categorias"
+					value={stats.categories}
+					icon={<FolderIcon className="h-6 w-6" />}
+					color="#d54d50"
+				/>
+				<Stat
+					title="Artigos"
+					value={stats.articles}
+					icon={<NewspaperIcon className="h-6 w-6" />}
+					color="#3bc480"
+				/>
+				<Stat
+					title="Usuários"
+					value={stats.users}
+					icon={<UserIcon className="h-6 w-6" />}
+					color="#3282cd"
+				/>
+			</div>
 		</div>
 	)
 }
